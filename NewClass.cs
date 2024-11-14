14d1aef53d11f425b6e33d9a9d4ccac86d8e0417
GGurBami <chkim716@gmail.com>
2024-11-14 13:15:02 +0900

    ➕ New Class

---
+++ NewClass.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class NewClass
+    {
+        public NewClass()
+        {
+            Console.Write("Test");
+        }
+    }
+}
